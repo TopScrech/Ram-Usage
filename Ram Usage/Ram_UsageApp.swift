15c9b1eb6823df64915b8c16dfe735997d3ef0e1
TopScrech <sergei_saliukov@icloud.com>
2024-08-14 00:17:13 +0200

    Initial Commit

---
+++ Ram Usage/Ram_UsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ram_UsageApp.swift
+//  Ram Usage
+//
+//  Created by Sergei Saliukov on 14/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Ram_UsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
