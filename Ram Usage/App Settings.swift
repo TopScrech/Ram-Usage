c06a5465a2db3d00a42aba7c8f9408f69d22ff2c
TopScrech <sergei_saliukov@icloud.com>
2024-09-05 11:19:29 +0200

    -

--- Ram Usage/App Settings.swift
+++ Ram Usage/App Settings.swift
@@ -1,31 +1,31 @@
-import SwiftUI
-
-struct AppSettings: View {
-    @Environment(RamVM.self) private var vm
-    
-    var body: some View {
-        List {
-            Text("Active \(vm.active)")
-            Text("AllFree \(vm.allFree)")
-            Text("allFreePercentage \(vm.allFreePercentage)")
-            Text("appMemory \(vm.appMemory)")
-            Text("cachedFiles \(vm.cachedFiles)")
-            Text("compressed \(vm.compressed)")
-            Text("free \(vm.free)")
-            Text("freeString \(vm.freeString)")
-            Text("inactive \(vm.inactive)")
-            Text("total \(vm.total)")
-            Text("usageHistory \(vm.usageHistory)")
-            Text("used \(vm.used)")
-            Text("usedPercentage \(vm.usedPercentage)")
-            Text("usedPercentageString \(vm.usedPercentageString)")
-            Text("usedString \(vm.usedString)")
-            Text("wired \(vm.wired)")
-        }
-    }
-}
-
-#Preview {
-    AppSettings()
-        .environment(RamVM())
-}
+//import SwiftUI
+//
+//struct AppSettings: View {
+//    @Environment(RamVM.self) private var vm
+//    
+//    var body: some View {
+//        List {
+//            Text("Active \(vm.active)")
+//            Text("AllFree \(vm.allFree)")
+//            Text("allFreePercentage \(vm.allFreePercentage)")
+//            Text("appMemory \(vm.appMemory)")
+//            Text("cachedFiles \(vm.cachedFiles)")
+//            Text("compressed \(vm.compressed)")
+//            Text("free \(vm.free)")
+//            Text("freeString \(vm.freeString)")
+//            Text("inactive \(vm.inactive)")
+//            Text("total \(vm.total)")
+//            Text("usageHistory \(vm.usageHistory)")
+//            Text("used \(vm.used)")
+//            Text("usedPercentage \(vm.usedPercentage)")
+//            Text("usedPercentageString \(vm.usedPercentageString)")
+//            Text("usedString \(vm.usedString)")
+//            Text("wired \(vm.wired)")
+//        }
+//    }
+//}
+//
+//#Preview {
+//    AppSettings()
+//        .environment(RamVM())
+//}
