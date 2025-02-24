0a4cb355a174e6ecbc4d9f9c08865b46ac1f7c72
TopScrech <sergei_saliukov@icloud.com>
2025-02-25 00:47:52 +0100

    iOS: FAQ

--- iOS/Ram Usage.swift
+++ iOS/Ram Usage.swift
@@ -1,10 +1,11 @@
 import ScrechKit
+import SafariCover
 
 @main
 struct RamUsage: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            AppContainer()
         }
     }
 }
