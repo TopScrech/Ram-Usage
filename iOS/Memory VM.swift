64f419bd2c76c5c60872b0ac83ca93fb5a125e6d
TopScrech <sergei_saliukov@icloud.com>
2025-02-25 08:14:13 +0100

    Improved way of fetching memory data

--- iOS/Memory VM.swift
+++ iOS/Memory VM.swift
@@ -11,28 +11,31 @@ final class MemoryVM {
     }
     
     func getMemoryUsage() {
-        var size = mach_msg_type_number_t(MemoryLayout<vm_statistics_data_t>.size / MemoryLayout<integer_t>.size)
+        var stats = vm_statistics64()
+        var size = mach_msg_type_number_t(MemoryLayout.size(ofValue: stats) / MemoryLayout<Int32>.stride)
         let host = mach_host_self()
-        var stats = vm_statistics_data_t()
         
         let status = withUnsafeMutablePointer(to: &stats) {
             $0.withMemoryRebound(to: integer_t.self, capacity: Int(size)) {
-                host_statistics(host, HOST_VM_INFO, $0, &size)
+                host_statistics64(host, HOST_VM_INFO64, $0, &size)
             }
         }
         
-        if status == KERN_SUCCESS {
-            let totalMemory = ProcessInfo.processInfo.physicalMemory
-            
-            let pageSize = vm_kernel_page_size
-            let usedMemory = (UInt64(stats.active_count) + UInt64(stats.inactive_count) + UInt64(stats.wire_count)) * UInt64(pageSize)
-            let freeMemory = totalMemory - usedMemory
-            
-            totalRam = formatBytes(totalMemory)
-            
-            usedRam = format(Int(totalMemory), Int(usedMemory))
-            freeRam = format(Int(totalMemory), Int(freeMemory))
+        guard status == KERN_SUCCESS else {
+            return
         }
+        
+        let totalMemory = ProcessInfo.processInfo.physicalMemory
+        var pageSize: vm_size_t = 0
+        host_page_size(host, &pageSize)
+        
+        let usedMemory = (UInt64(stats.active_count) + UInt64(stats.wire_count)) * UInt64(pageSize)
+        let freeMemory = totalMemory - usedMemory
+        
+        totalRam = formatBytes(totalMemory)
+        
+        usedRam = format(Int(totalMemory), Int(usedMemory))
+        freeRam = format(Int(totalMemory), Int(freeMemory))
     }
     
     private func format(_ total: Int, _ value: Int) -> String {
